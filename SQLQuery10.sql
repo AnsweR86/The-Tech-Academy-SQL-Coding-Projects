a822ba2c48a039bd7505969bf32efe920cae6e11
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2019-01-08 04:42:57 -0800

    Correction on Insert Statement

--- SQLQuery10.sql
+++ SQLQuery10.sql
@@ -50,7 +50,7 @@ BEGIN
 		(book_title, book_Publisher)
 		VALUES 
 		('The Lost Tribe','Kyate Corp.'),
-		('The Great Gatsby','Udnt'),
+		('The Great Gatsby2','Udnt'),
 		('To Kill a Mockingbird','Pheonix'),
 		('Beloved','The Azure'),
 		('Lord of Filies','Willy utd.'),
@@ -80,83 +80,161 @@ BEGIN
 		INSERT INTO tbl_borrower
 		(borrower_cardNo,borrower_name,borrower_phone,borrower_address)
 		VALUES
-		('756','Edwardo Marriotted','343-434-3433','343 smith st..'),
-		('433','Scott Fizgerd','434-434-7676','453 highsuit pl..'),
-		('434','Harper Leweis','432-766-7643','9000 45th ave..'),
-		('345','Bob White','434-453-9066','850 P.O Box..'),
-		('434','Joegl Hellir','434-879-6565','4456 mariotte st.'),
-		('543','Jose Cond','435-464-5466','4353 potter st..'),
-		('806','James Joy','546-656-5454','777 goose ave..'),
-		('545','Emily Dicki','343-454-5454','546 birsing st..'),
-		('656','Tony Morr','335-657-5665','546 smitter ave..'),
-		('546','Jon Uphike','454-545-5454','545 goash ave..');
+		('100','Edwardo Marriotted','343-434-3433','343 smith st..'),
+		('101','Scott Fizgerd','434-434-7676','453 highsuit pl..'),
+		('102','Harper Leweis','432-766-7643','9000 45th ave..'),
+		('103','Bob White','434-453-9066','850 P.O Box..'),
+		('104','Joegl Hellir','434-879-6565','4456 mariotte st.'),
+		('105','Jose Cond','435-464-5466','4353 potter st..'),
+		('106','James Joy','546-656-5454','777 goose ave..'),
+		('107','Emily Dicki','343-454-5454','546 birsing st..'),
+		('108','Tony Morr','335-657-5665','546 smitter ave..'),
+		('109','Jon Uphike','454-545-5454','545 goash ave..');
 
 
 
 	CREATE TABLE tbl_bookAuthor(
-		bookAuthor_bookID INT PRIMARY KEY NOT NULL IDENTITY (101,1),
+		bookAuthor_bookID INT PRIMARY KEY NOT NULL IDENTITY (10,1),
 		bookAuthor_authorName VARCHAR (50) NOT NULL,
 		bookAuthor_book INT NOT NULL CONSTRAINT fk_bookID FOREIGN KEY REFERENCES tbl_book(book_id) ON UPDATE CASCADE ON DELETE CASCADE);
 
 
 		INSERT INTO tbl_bookAuthor
-		(bookAuthor_authorName)
+		(bookAuthor_bookID, bookAuthor_authorName)
 		VALUES
-		('Edward Marriott'),
-		('F.Scott Fitzgerald'),
-		('Harper Lee'),
-		('E.B White'),
-		('Joe Heller'),
-		('Joseph Conrad'),
-		('James Joyce'),
-		('Emily Dickinson'),
-		('Toni Morrison'),
-		('John Updike'),
-		('Stephen King'),
-		('Stephen King');
+		('13','Edward Marriott'),
+		('11','F.Scott Fitzgerald'),
+		('12','Harper Lee'),
+		('9','E.B White'),
+		('8','Joe Heller'),
+		('7','Joseph Conrad'),
+		('6','James Joyce'),
+		('5','Emily Dickinson'),
+		('4','Toni Morrison'),
+		('3','John Updike'),
+		('2','Stephen King'),
+		('1','Stephen King');
+		
 
 	CREATE TABLE tbl_bookCopies(
 		bookCopies_bookID INT PRIMARY KEY NOT NULL IDENTITY (101,1),
 		bookCopies_book INT NOT NULL CONSTRAINT fk_bookName FOREIGN KEY  REFERENCES  tbl_book(book_id)ON UPDATE CASCADE ON DELETE CASCADE,
-		bookCopies_library_branch INT NOT NULL CONSTRAINT fk_branchIdentification FOREIGN KEY REFERENCES tbl_library_branch(branch_id)ON UPDATE CASCADE ON DELETE CASCADE,
-		bookCopies_Number_of_Copies INT NOT NULL
-		);
-
+		bookCopies_library_branchID INT NOT NULL CONSTRAINT fk_branchIdentification FOREIGN KEY REFERENCES tbl_library_branch(branch_id)ON UPDATE CASCADE ON DELETE CASCADE,
+		bookCopies_Number_of_Copies INT NOT NULL);
+		
 			
 		INSERT INTO tbl_bookCopies
-		(bookCopies_bookID,bookCopies_library_branch,bookCopies_Number_of_Copies)
+		(bookCopies_bookID,bookCopies_library_branchID,bookCopies_Number_of_Copies)
 		VALUES
-		('10','2','2'),
-		('9','1','3'),
-		('8','1','4'),
-		('7','2','5'),
-		('6','2','4'),
-		('5','2','3'),
-		('4','3','2'),
-		('3','4','7'),
-		('2','3','2'),
-		('1','3','5');
+		('10','1','2'),
+		('11','1','3'),
+		('12','1','4'),
+		('13','1','5'),
+		('14','1','4'),
+		('15','1','3'),
+		('16','1','2'),
+		('17','1','7'),
+		('18','1','2'),
+		('19','1','5'),
+		('20','2','2'),
+		('21','2','3'),
+		('22','2','4'),
+		('23','2','5'),
+		('24','2','4'),
+		('25','2','3'),
+		('26','2','2'),
+		('27','2','7'),
+		('28','2','2'),
+		('29','2','5'),
+		('10','3','2'),
+		('11','3','3'),
+		('12','3','4'),
+		('13','3','5'),
+		('14','3','4'),
+		('15','3','3'),
+		('16','3','2'),
+		('17','3','7'),
+		('18','3','2'),
+		('19','3','5'),
+		('20','4','2'),
+		('21','4','3'),
+		('22','4','4'),
+		('23','4','5'),
+		('24','4','4'),
+		('25','4','3'),
+		('26','4','2'),
+		('27','4','7'),
+		('28','4','2'),
+		('29','4','5'),
+		('1','2','2'),
+		('2','2','2');
 
 	CREATE TABLE tbl_bookLoan(
 		bookLoan_bookID INT PRIMARY KEY NOT NULL IDENTITY (20,1),
-		bookLoan_library_branch INT NOT NULL CONSTRAINT fk_branch_id FOREIGN KEY REFERENCES tbl_library_branch(branch_id)ON UPDATE CASCADE ON DELETE CASCADE,
-		bookLoan_borrower INT NOT NULL CONSTRAINT fk_cardNo FOREIGN KEY REFERENCES tbl_borrower(borrower_cardNo)ON UPDATE CASCADE ON DELETE CASCADE,
+		bookLoan_library_branchID INT NOT NULL CONSTRAINT fk_branch_id FOREIGN KEY REFERENCES tbl_library_branch(branch_id)ON UPDATE CASCADE ON DELETE CASCADE,
+		bookLoan_CardNo INT NOT NULL CONSTRAINT fk_cardNo FOREIGN KEY REFERENCES tbl_borrower(borrower_cardNo)ON UPDATE CASCADE ON DELETE CASCADE,
 		bookLoan_DateOut DATE  NULL, bookLoan_DateDue DATE  NULL);
 	 
 		 
 		
 	
 		INSERT INTO tbl_bookLoan
-		(bookLoan_borrower, bookLoan_library_branch,bookLoan_DateOut,bookLoan_DateDue)
+		(bookLoan_bookID,bookLoan_library_branchID,bookLoan_DateOut,bookLoan_DateDue)
 		VALUES
-		('Harper Dew', '1' ,'01/03/2015' ,'01/09/2015'),
-		('Juinor White ' ,'2' ,'12/23/2014' ,'12/27/2014'),
-		('Hitler Weet', '3', '01/01/2015', '01/11/2015'),
-		('Jhon Conrad', '4' ,'01/05/2015','01/12/2015'),
-		('Joy Ride','5','01/07/2015','01/14/2015'),
-		('Yoloo Park','6','01/15/2015','01/24/2015'),
-		('Nink Impossible','7','01/17/2015','01/25/2015'),
-		('Sunset Uprising','8','02/02/2014','04/05/2015');
+		('21','1','100' ,'01/03/2015' ,'01/09/2015'),
+		('22 ' ,'1','100' ,'12/23/2014' ,'12/27/2014'),
+		('23', '1','100', '01/01/2015', '01/11/2015'),
+		('24', '1','100' ,'01/05/2015','01/12/2015'),
+		('25','1','100','01/07/2015','01/14/2015'),
+		('26','2','101','01/15/2015','01/24/2015'),
+		('27','2','101','01/17/2015','01/25/2015'),
+		('28','2','101','02/02/2014','04/05/2015'),
+		('12','2','101','04/04/2014','04/09/2014'),
+		('13','2','101','06/07/2014','06/18/2014'),
+		('21','1','102' ,'01/03/2015' ,'01/09/2015'),
+		('22 ' ,'1','102' ,'12/23/2014' ,'12/27/2014'),
+		('23', '1','102', '01/01/2015', '01/11/2015'),
+		('24', '1','102' ,'01/05/2015','01/12/2015'),
+		('25','1','103','01/07/2015','01/14/2015'),
+		('26','2','103','01/15/2015','01/24/2015'),
+		('27','2','103','01/17/2015','01/25/2015'),
+		('28','2','103','02/02/2014','04/05/2015'),
+		('12','2','103','04/04/2014','04/09/2014'),
+		('13','2','103','06/07/2014','06/18/2014'),
+		('21','3','104' ,'01/03/2015' ,'01/09/2015'),
+		('22 ' ,'3','104' ,'12/23/2014' ,'12/27/2014'),
+		('23', '3','104', '01/01/2015', '01/11/2015'),
+		('24', '3','104' ,'01/05/2015','01/12/2015'),
+		('25','3','104','01/07/2015','01/14/2015'),
+		('26','3','104','01/15/2015','01/24/2015'),
+		('27','3','104','01/17/2015','01/25/2015'),
+		('28','3','105','02/02/2014','04/05/2015'),
+		('12','3','105','04/04/2014','04/09/2014'),
+		('13','3','105','06/07/2014','06/18/2014'),
+		('21','1','105' ,'01/03/2015' ,'01/09/2015'),
+		('22 ' ,'1','105' ,'12/23/2014' ,'12/27/2014'),
+		('23', '1','106', '01/01/2015', '01/11/2015'),
+		('24', '1','106' ,'01/05/2015','01/12/2015'),
+		('25','1','106','01/07/2015','01/14/2015'),
+		('26','2','106','01/15/2015','01/24/2015'),
+		('27','2','106','01/17/2015','01/25/2015'),
+		('28','2','106','02/02/2014','04/05/2015'),
+		('12','2','101','04/04/2014','04/09/2014'),
+		('13','2','101','06/07/2014','06/18/2014'),
+		('21','1','108' ,'01/03/2015' ,'01/09/2015'),
+		('22 ' ,'1','107' ,'12/23/2014' ,'12/27/2014'),
+		('23', '1','107', '01/01/2015', '01/11/2015'),
+		('24', '1','107' ,'01/05/2015','01/12/2015'),
+		('25','1','107','01/07/2015','01/14/2015'),
+		('26','2','107','01/15/2015','01/24/2015'),
+		('27','2','107','01/17/2015','01/25/2015'),
+		('28','2','108','02/02/2014','04/05/2015'),
+		('12','2','108','04/04/2014','04/09/2014'),
+		('13','2','108','06/07/2014','06/18/2014');
+
+
+
+
 
 		
 	END
