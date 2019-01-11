6ce24e426736f3a5f08d982b05509b77964289c2
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2019-01-11 12:43:05 -0800

    NAMING AND SPACING CORRECTIONS

--- SQLQuery111.sql
+++ SQLQuery111.sql
@@ -18,59 +18,59 @@ GO
 -- Create date: Today
 -- Description:	QuanityofBookinSharpstown
 -- =============================================
-CREATE PROCEDURE bookTHELOSTTRIBEinsharpstown 
-	-- Add the parameters for the stored procedure here
-	@Lastname nvarchar(50) = NULL, 
-	@FirstName nvarchar(50) = NULL
+
+CREATE PROC[dbo].[LibraryInventory]
+ 
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
-	SELECT Count (bookCopies_Number_of_Copies) AS 'NumberofCopies'
-	FROM tbl_book,tbl_bookCopies,tbl_library_branch
-	WHERE book_title ='The Lost Tribe' AND branch_name='Sharpstown'
-
-	SELECT bookCopies_library_branchID,COUNT (bookCopies_Number_of_Copies) AS 'NumerofCopies The Lost Tribe'
-	FROM tbl_book,tbl_bookCopies
-	WHERE book_title='The Lost Tribe'
-	GROUP BY bookCopies_library_branchID
-
-	SELECT borrower_name AS 'Name'
-	FROM tbl_borrower a1 LEFT JOIN tbl_bookLoan a2 ON a2.bookLoan_CardNo = a1.borrower_cardNo
-	WHERE a2.bookLoan_bookID is NULL
-
-	SELECT a1.book_title, a2.borrower_Name, a2.borrower_address
-	FROM tbl_borrower a2
-	INNER JOIN tbl_bookLoan a3 ON a3.bookLoan_CardNo=a2.borrower_cardNo
-	INNER JOIN tbl_book a1 ON a1.book_id=a3.bookLoan_bookID
-	WHERE a3.bookLoan_DateDue= CAST(GETDATE() AS DATE) AND
-	a3.bookLoan_library_branchID=1;
-
-	SELECT a1.branch_name AS 'Branch Name', COUNT(a2.bookLoan_bookID) AS 'Number of Books Loaned'
-	FROM tbl_bookLoan a2
-	INNER JOIN tbl_library_branch a1 ON a1.branch_id=a2.bookLoan_library_branchID
-	GROUP BY a1.branch_name
-
-
-	SELECT a1.borrower_name AS 'Name', a1.borrower_address AS 'Borrower''s Address', COUNT(a2.bookloan_CardNo) AS 'Number of Books'
-	FROM tbl_borrower a1
-	INNER JOIN tbl_bookLoan a2 ON a2.bookLoan_CardNo=a1.borrower_cardNo
-	GROUP BY a1.borrower_name, a1.borrower_address
-	HAVING COUNT(a2.bookloan_CardNo)>5 
-	ORDER BY a1.borrower_Name
-
-	SELECT a1.book_title AS 'Title of Book' , COUNT(*) AS 'Quanity of book'
-	FROM tbl_bookAuthor a2 INNER JOIN tbl_bookAuthor a3 ON a3.bookAuthor_authorName=a2.bookAuthor_authorName
-	INNER JOIN tbl_bookCopies a4 ON a4.bookCopies_bookID = a2.bookAuthor_bookID
-	INNER JOIN tbl_book a1 ON a1.book_id = a1.book_id
-	INNER JOIN tbl_library_branch a5 ON a5.branch_id = a4.bookCopies_library_branchID
-	WHERE a2.bookAuthor_authorName='Stephen King' AND a4.bookCopies_library_branchID='2'
-	GROUP BY a1.book_title
 	
+--COUNTING 'THE LOST TRIBE' IN SHARPSTOWN	
+SELECT Count (bookCopies_Number_of_Copies) AS 'Number of book called ''THE LOST TRIBE'' in Sharpstown'
+FROM tbl_book,tbl_bookCopies,tbl_library_branch
+WHERE book_title ='The Lost Tribe' AND branch_name='Sharpstown'
 
+--COUNTING 'THE LOST TRIBE' IN EVERY BRANCH
+SELECT bookCopies_library_branchID AS 'Branchs of Library',COUNT (bookCopies_Number_of_Copies) AS 'Quanity of book called ''The Lost Tribe''in each Branch'
+FROM tbl_book,tbl_bookCopies
+WHERE book_title='The Lost Tribe'
+GROUP BY bookCopies_library_branchID 
+
+--Clints with Checkout books
+SELECT borrower_name AS 'Names of Current active clints'
+FROM tbl_borrower a1 LEFT JOIN tbl_bookLoan a2 ON a2.bookLoan_CardNo = a1.borrower_cardNo
+WHERE a2.bookLoan_bookID is NULL
+
+--DUEDATE CLINTS info.
+SELECT a1.book_title AS'Name of Book Due' ,a2.borrower_Name , a2.borrower_address
+FROM tbl_borrower a2
+INNER JOIN tbl_bookLoan a3 ON a3.bookLoan_CardNo=a2.borrower_cardNo
+INNER JOIN tbl_book a1 ON a1.book_id=a3.bookLoan_bookID
+WHERE a3.bookLoan_DateDue= CAST(GETDATE() AS DATE) AND
+a3.bookLoan_library_branchID=1;
+
+--Book checkout for every branch
+SELECT a1.branch_name AS 'Branch Name', COUNT(a2.bookLoan_bookID) AS 'Number of Books Loaned'
+FROM tbl_bookLoan a2
+INNER JOIN tbl_library_branch a1 ON a1.branch_id=a2.bookLoan_library_branchID
+GROUP BY a1.branch_name
+
+
+--CLINTS with 5book or more
+SELECT a1.borrower_name AS 'Name', a1.borrower_address AS 'Borrower''s Address', COUNT(a2.bookloan_CardNo) AS 'Number of Books'
+FROM tbl_borrower a1
+INNER JOIN tbl_bookLoan a2 ON a2.bookLoan_CardNo=a1.borrower_cardNo
+GROUP BY a1.borrower_name, a1.borrower_address
+HAVING COUNT(a2.bookloan_CardNo)>5 
+ORDER BY a1.borrower_Name
+
+--book by stephen King in Central
+SELECT a1.book_title AS 'Title of Book by Stepeh King', COUNT(*) AS 'Quanity of book'
+FROM tbl_bookAuthor a2 INNER JOIN tbl_bookAuthor a3 ON a3.bookAuthor_authorName=a2.bookAuthor_authorName
+INNER JOIN tbl_bookCopies a4 ON a4.bookCopies_bookID = a2.bookAuthor_bookID
+INNER JOIN tbl_book a1 ON a1.book_id = a1.book_id
+INNER JOIN tbl_library_branch a5 ON a5.branch_id = a4.bookCopies_library_branchID
+WHERE a2.bookAuthor_authorName='Stephen King' AND a4.bookCopies_library_branchID='2' or a5.branch_name='Central'
+GROUP BY a1.book_title
 
 
 
